56df85b41ee9090ae0886ba9f73c9547becb64ac
DarkPonyBee <thomas.kenzo716@gmail.com>
2020-07-26 02:29:15 +1000

    searchlist show as position absolute

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,81 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import { Auth } from "aws-amplify";
 import { NotificationManager } from "react-notifications";
 
 const StyledSearchBarContainer = styled.div`
+  position: relative;
   margin: 35px auto auto;
   max-width: 1196px;
-  padding: 0px 45px;
-  border: 1px solid #979797;
-  border-radius: 40px;
-  background-color: #ffffff;
-  box-shadow: 0 8px 16px 0 #e0e2e4;
   .searchbar {
-    display: flex;
+    box-shadow: ${(props) =>
+      props.searchList ? "0 0px 8px 0 #e0e2e4" : "0 8px 16px 0 #e0e2e4"};
+    background-color: #ffffff;
+    padding: 0px 45px;
     height: 80px;
-    input {
+    border: 1px solid #979797;
+    border-radius: ${(props) =>
+      props.searchList ? "15px 15px 0px 0px" : "40px"};
+    ${(props) => (props.searchList ? "border-bottom: none" : "")};
+    &-container {
+      display: flex;
       height: 100%;
-      width: 100%;
-      outline: none;
-      border: none;
-      color: rgba(0, 0, 0, 0.65);
-      font-size: 24px;
-      letter-spacing: 0;
-      line-height: 26px;
-    }
-    &-icon {
-      width: 35px;
-      height: 35px;
-      margin: auto 0px auto 20px;
-      &:hover {
-        cursor: pointer;
-        opacity: 0.8;
+      ${(props) =>
+        props.searchList ? "border-bottom: 1px solid #DADCE3" : ""};
+      input {
+        height: 100%;
+        width: 100%;
+        outline: none;
+        border: none;
+        color: rgba(0, 0, 0, 0.65);
+        font-size: 24px;
+        letter-spacing: 0;
+        line-height: 26px;
       }
-    }
-    &-closeicon {
-      width: 35px;
-      height: 35px;
-      margin: auto 0px;
-      padding-right: 20px;
-      border-right: 1px solid #cccccb;
-      opacity: 0.8;
-      &:hover {
-        cursor: pointer;
-        opacity: 1;
+      &-icon {
+        width: 35px;
+        height: 35px;
+        margin: auto 0px auto 20px;
+        &:hover {
+          cursor: pointer;
+          opacity: 0.8;
+        }
+      }
+      &-closeicon {
+        width: 35px;
+        height: 35px;
+        margin: auto 0px;
+        padding-right: 20px;
+        border-right: 1px solid #cccccb;
+        opacity: 0.8;
+        &:hover {
+          cursor: pointer;
+          opacity: 1;
+        }
       }
     }
   }
   .searchbar-list {
-    border-top: 1px solid #dadce3;
-    padding: 20px 0px;
+    position: absolute;
+    width: 100%;
+    background-color: #ffffff;
+    box-shadow: ${(props) =>
+      props.searchList ? "0 8px 8px 0 #e0e2e4" : "0 8px 16px 0 #e0e2e4"};
+    border: 1px solid #979797;
+    border-top: none;
+    border-radius: 0px 0px 15px 15px;
+    padding: 20px 45px;
   }
   @media only screen and (max-width: 1240px) {
     width: 90%;
   }
   @media only screen and (max-width: 712px) {
-    padding: 0px 25px;
     .searchbar {
+      padding: 0px 25px;
       height: 50px;
-      input {
-        font-size: 18px;
+      &-container {
+        input {
+          font-size: 18px;
+        }
+        &-icon {
+          width: 25px;
+          height: 25px;
+          margin-left: 10px;
+        }
+        &-closeicon {
+          width: 25px;
+          height: 25px;
+          padding-right: 10px;
+        }
       }
-      &-icon {
-        width: 25px;
-        height: 25px;
-        margin-left: 10px;
-      }
-      &-closeicon {
-        width: 25px;
-        height: 25px;
-        padding-right: 10px;
-      }
+    }
+    .searchbar-list {
+      padding: 20px 25px;
     }
   }
 `;
 
 const SearchBar = () => {
+  const [searchList, setSearchList] = useState(false);
   let debounce = null;
 
   const onSearch = async (query) => {
@@ -110,7 +132,11 @@ const SearchBar = () => {
 
   const onInputChange = (e) => {
     const searchQuery = e.target.value;
-    if (!searchQuery) return;
+    if (!searchQuery) {
+      setSearchList(false);
+      return;
+    }
+    setSearchList(true);
     clearTimeout(debounce);
     debounce = setTimeout(() => {
       onSearch(searchQuery);
@@ -118,18 +144,31 @@ const SearchBar = () => {
   };
 
   return (
-    <StyledSearchBarContainer>
+    <StyledSearchBarContainer searchList={searchList}>
       <div className="searchbar">
-        <input
-          type="text"
-          name="searchQuery"
-          placeholder="Search for files, emails, tasks and much more"
-          onChange={onInputChange}
-        ></input>
-        {/* <ion-icon name="close-outline" class="searchbar-closeicon"></ion-icon> */}
-        <ion-icon name="search-outline" class="searchbar-icon"></ion-icon>
+        <div class="searchbar-container">
+          <input
+            type="text"
+            name="searchQuery"
+            placeholder="Search for files, emails, tasks and much more"
+            onChange={onInputChange}
+          ></input>
+          {searchList && (
+            <ion-icon
+              name="close-outline"
+              class="searchbar-container-closeicon"
+              onClick={() => {
+                setSearchList(false);
+              }}
+            ></ion-icon>
+          )}
+          <ion-icon
+            name="search-outline"
+            class="searchbar-container-icon"
+          ></ion-icon>
+        </div>
       </div>
-      {/* <div className="searchbar-list"></div> */}
+      {searchList && <div className="searchbar-list">dfghfgh</div>}
     </StyledSearchBarContainer>
   );
 };
